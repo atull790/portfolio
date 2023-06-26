5582fb23f2e7de21b6317a861086710905846ecd
atull790 <137768813+atull790@users.noreply.github.com>
2023-06-26 17:05:47 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -140,8 +140,8 @@ LOGIN_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build' 'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 django_heroku.settings(locals())
 
